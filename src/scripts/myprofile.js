9103816bc3f8442b120ccdcdb3ed7e0286dff8cc
saikrishnakoleti <saikrishna.koleti@gmail.com>
2018-07-05 20:07:04 +0530

    React Examples

---
+++ src/scripts/myprofile.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class MyProfile extends Component {
+  render() {
+    return (
+      <div>
+        <h2>This is MyProfile</h2>
+        <p> Hello World </p>
+      </div>
+    );
+  }
+}
+
+export default MyProfile;
